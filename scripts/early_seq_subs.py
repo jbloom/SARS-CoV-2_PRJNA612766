a7a2856b09694bb6ecb3c1cfdb088e8074698c3e
jbloom <jbloom@fredhutch.org>
2021-05-31 11:03:37 -0700

    more info in early seq substitutions

--- scripts/early_seq_subs.py
+++ scripts/early_seq_subs.py
@@ -12,16 +12,26 @@ print(f"Reference genome is {ref_genome.id}")
 length = len(ref_genome)
 ref_seqstr = str(ref_genome.seq).upper()
 
+start = snakemake.params.region_of_interest['start']
+end = snakemake.params.region_of_interest['end']
+print(f"Annotating frac sites called in region of interest: {start} to {end}")
+assert 1 <= start <= end <= length
+
 print(f"Reading aligned sequences from {snakemake.input.alignment}")
 iseq = 0
 data = {'strain': [],
         'date': [],
+        'country': [],
+        'location': [],
+        'age': [],
+        'sex': [],
         'gisaid_epi_isl': [],
         'genbank_accession': [],
         'n_gapped_to_ref': [],
         'n_ident_to_ref': [],
         'n_subs_to_ref': [],
         'n_ambiguous_to_ref': [],
+        'frac_called_in_region_of_interest': [],
         'substitutions': [],
         }
 for seq in Bio.SeqIO.parse(snakemake.input.alignment, 'fasta'):
@@ -31,6 +41,9 @@ for seq in Bio.SeqIO.parse(snakemake.input.alignment, 'fasta'):
     else:
         assert len(seq) == length
         seqstr = str(seq.seq).upper()
+        data['frac_called_in_region_of_interest'].append(
+                sum(nt in {'A', 'C', 'G', 'T'} for nt in
+                    seq[start - 1: end]) / (end - start + 1))
         data['n_gapped_to_ref'].append(seqstr.count('-'))
         data['n_ident_to_ref'].append(sum(ref_nt == seq_nt for ref_nt, seq_nt
                                           in zip(ref_seqstr, seqstr)))
@@ -45,10 +58,10 @@ for seq in Bio.SeqIO.parse(snakemake.input.alignment, 'fasta'):
                                                        'n_subs_to_ref',
                                                        'n_ambiguous_to_ref'])
         data['substitutions'].append(','.join(subs))
-        head_entries = seq.description.split()
-        data['strain'].append(head_entries[0])
-        for entry in head_entries[1:]:
-            assert entry.count('=') == 1
+        strain, head_entries = seq.description.split(' ', 1)
+        data['strain'].append(strain)
+        for entry in head_entries.split(', '):
+            assert entry.count('=') == 1, seq.description
             key, val = entry.split('=')
             data[key].append(val)
     iseq += 1
