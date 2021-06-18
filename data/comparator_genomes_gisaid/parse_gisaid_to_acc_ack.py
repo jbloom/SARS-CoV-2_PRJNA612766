93471b1333ef4ec153be142548b1ee154ff3e39b
jbloom <jbloom@fredhutch.org>
2021-06-17 17:04:49 -0700

    add GISAID acknowledgments and accessions

---
+++ data/comparator_genomes_gisaid/parse_gisaid_to_acc_ack.py
@@ -0,0 +1,31 @@
+"""Parse xzipped GISAID TSV to accessions and acknowledgments."""
+
+
+import lzma
+
+import pandas as pd
+
+
+with lzma.open('1622468911409.metadata.tsv.xz') as f:
+    df = (pd.read_csv(f, sep='\t')
+          # next two lines handle entries with newlines
+          .query('gisaid_epi_isl.notnull()')
+          .query('gisaid_epi_isl.str.startswith("EPI_ISL_")')
+          .reset_index(drop=True)
+          )
+
+accessions = df['gisaid_epi_isl'].tolist()
+assert len(accessions) == len(set(accessions))
+acc_file = 'accessions.txt'
+print(f"Writing the {len(accessions)} to {acc_file}")
+with open(acc_file, 'w') as f:
+    f.write('\n'.join(accessions))
+
+acknowledgments = (
+    df
+    [['originating_lab', 'submitting_lab', 'authors']]
+    .drop_duplicates()
+    )
+ack_file = 'acknowledgments.txt'
+print(f"Writing the {len(acknowledgments)} to {ack_file}")
+acknowledgments.to_csv(ack_file, index=False)
